d548b1b713cc54cd1743a1ec99eef7a86fc42ada
Glydewell Burdick <glydewellb@gmail.com>
2021-05-26 21:40:26 -0500

    add password complexity validation

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,4 +4,15 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
   belongs_to :account
+
+  validate :password_complexity
+
+  def password_complexity
+    return if password.blank?
+
+    errors.add :password, I18n.t('users.errors.password_too_short') if password.length < 8
+    errors.add :password, I18n.t('users.errors.password_no_number') unless password =~ /[0-9]/
+    errors.add :password, I18n.t('users.errors.password_no_uppercase') unless password =~ /[[:upper:]]/
+    errors.add :password, I18n.t('users.errors.password_no_lowercase') unless password =~ /[[:lower:]]/
+  end
 end
